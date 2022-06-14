c87de23096422ddc3bc04f7a4d77acbbe34e8aa3
Aaron Olsen <aaron.olsen@nextworld.net>
2022-06-14 14:52:25 -0600

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const node = document.createElement("div");
-const textnode = document.createTextNode("Water");
+const textnode = document.createTextNode("Hello World");
 node.appendChild(textnode);
 
 document.getElementById("root").appendChild(node);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = () => {
     return {
+        mode: "development",
         entry: './src/index.js',
         output: {
             filename: 'main.js',
             path: path.resolve(__dirname, 'dist'),
-       }
+       },
+       plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'src', 'index.html'),
+            })
+       ]
     }
 }
